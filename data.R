7ced717b819ca2386ac0ae4abd588b25a03ccdad
tdienlin <tobias.dienlin@uni-hohenheim.de>
2019-07-30 16:49:35 +0200

    - adding power analyses for studies typically run in Communication

---
+++ data.R
@@ -0,0 +1,7 @@
+Topic area, Number of metaanalyses, Mean r, SD r, Median r, Mean k, SD k, Median k, Mean n, SD n, Median n
+Persuasion, 37, .18, (.19), .13, 51, (48), 38, 10,213, (16,697), 4269
+Media, 35, .20, (.11), .18, 69, (189), 25, 10,889, (20,882), 5456
+Interpersonal, 34, .24, (.15), .20, 37, (38), 22, 18,445, (67,422), 3631
+Instructional, 23, .24, (.15), .22, 44, (37), 29, 16,069, (23,131), 6206
+Health, 15, .18, (.13), .14, 34, (31), 23, 493,172, (1,771,956), 5866
+Organizational/group, 5, .25, (.18), .22, 53, (69), 19, 2565, (2497), 1956
